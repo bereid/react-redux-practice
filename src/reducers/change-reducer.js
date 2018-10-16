2d01f062ef4b3f84bb5166918687d2953751bd09
Daniel Berei <berei.daniel@gmail.com>
2018-10-16 15:51:59 +0200

    counter cant go under zero

--- src/reducers/change-reducer.js
+++ src/reducers/change-reducer.js
@@ -5,7 +5,7 @@ const defaultStore = {
 const changeReducer = (store = defaultStore, action) => {
   switch (action.type) {
     case 'DECREMENT':
-      return Object.assign({}, store, { value: store.value - action.count });
+      return Object.assign({}, store, { value: store.value > 0 ? store.value - action.count : 0 });
     case 'INCREMENT':
       return Object.assign({}, store, { value: store.value + action.count });
     default:
